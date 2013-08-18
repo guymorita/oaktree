3bb83109cfeafeffe996885a75ddae00b47a898b
cmdoptesc <cmdoptesc@users.noreply.github.com>
2013-08-17 18:49:31 -0700

    Add send message tests.

--- test/testOaktree.js
+++ test/testOaktree.js
@@ -66,3 +66,45 @@ describe('User login', function(){
     });
   });
 });
+
+describe('Messages', function(){
+  var message_id = 1;
+  beforeEach(function(done){
+    oaktree.Message.find().remove({});
+    request(oaktree.server)
+      .get('/message/send/132/555/helloworld')
+      .end(function(err, res){
+        message_id = res.body._id;
+        done();
+      });
+  });
+  it('should embed the correct sender id to the message', function(done){
+    // oaktree.User.findOne({name:'bob'}, function(err, res){
+      oaktree.Message.findOne({_id: message_id }, function(err, item) {
+        assert.equal(item.sender_id, 132);
+        done();
+      });
+  });
+  it('should embed the correct receiver id to the message', function(done){
+    // oaktree.User.findOne({name:'bob'}, function(err, res){
+      oaktree.Message.findOne({_id: message_id }, function(err, item) {
+        assert.equal(item.receiver_id, 555);
+        done();
+      });
+  });
+  it('should embed the correct message', function(done){
+    // oaktree.User.findOne({name:'bob'}, function(err, res){
+      oaktree.Message.findOne({_id: message_id }, function(err, item) {
+        assert.equal(item.message_body, "helloworld");
+        done();
+      });
+  });
+  // it('should embed the correct receipient id to the message', function(done){
+  //   request(oaktree.server)
+  //     .get('/user/login/bob/bobpassddd')
+  //     .end(function(err, res){
+  //       assert.equal(res.statusCode, "401");
+  //       done();
+  //   });
+  // });
+});
