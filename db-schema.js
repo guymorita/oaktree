2ec3a7d45d9ecf89ee7582c387d490654691b5ae
cmdoptesc <cmdoptesc@users.noreply.github.com>
2013-08-17 17:07:57 -0700

    Update db schema for messages.

--- db-schema.js
+++ db-schema.js
@@ -3,11 +3,30 @@ exports.mongodb = function(){
   var mongoose = require('mongoose');
 
   var userSchema = mongoose.Schema({
-    name: {type: String, index: { unique: true }},
+    name: {type: String, lowercase: true, trim: true, index: { unique: true }},
     password: String
   });
 
   var User = mongoose.model('User', userSchema);
 
-  return { User: User };
+  var messageSchema = mongoose.Schema({
+    sender_id: String,
+    receiver_id: String,
+    status: { type: Number, default: 0 },
+    geo_lat: Number,
+    geo_lon: Number,
+    hidden: Boolean,
+    cleared: { type: Boolean, default: false },
+    title: String,
+    message_body: String,
+    pic_url: String,
+    date: { type: Date, default: Date.now }
+  });
+
+  var Message = mongoose.model('User', messageSchema);
+
+  return {
+    User: User,
+    Message: Message
+  };
 };
