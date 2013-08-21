a59e4ce02ad0baedd9ad4cbadbe55fc7568f9bb5
cmdoptesc <cmdoptesc@users.noreply.github.com>
2013-08-20 17:41:01 -0700

    Add messages for init test.

--- test/init-for-test.js
+++ test/init-for-test.js
@@ -34,18 +34,66 @@ var makeMessages = function(users) {
   var message = {
     sender_id: users[0].id,
     receiver_ids: [users[1].id, users[2].id, users[3].id],
-    content: "omfg how r u??",
-    title: "hello test"
+    content: "message to tom, sally, and jill",
+    title: "from bob, japantown test",
+    latlng: {"lat":37.785385,"lng":-122.429747}
   };
-
   request(oaktree.server).post('/message')
-    // .set('Content-Type', 'application/json')
     .send(JSON.stringify(message))
     .end(function(err, res){
       if (err) {
         console.log("post err", err);
         throw new err();
       }
+
+      message = {
+        sender_id: users[1].id,
+        receiver_ids: [users[0].id, users[3].id],
+        content: "message to bob and jill.. at dolores park",
+        title: "from tom, dolores park",
+        latlng: {"lat":37.760317,"lng":-122.426845}
+      };
+      request(oaktree.server).post('/message')
+        .send(JSON.stringify(message))
+        .end(function(err, res){
+
+          message = {
+            sender_id: users[2].id,
+            receiver_ids: [users[3].id],
+            content: "message to jill",
+            title: "from sally, nob hill",
+            latlng: {"lat":37.792355,"lng":-122.411889}
+          };
+          request(oaktree.server).post('/message')
+            .send(JSON.stringify(message))
+            .end(function(err, res){
+
+              message = {
+                sender_id: users[2].id,
+                receiver_ids: [users[3].id],
+                content: "second message to jill",
+                title: "2nd from sally, hack reactor",
+                latlng: {"lat":37.783715,"lng":-122.408976}
+              };
+              request(oaktree.server).post('/message')
+                .send(JSON.stringify(message))
+                .end(function(err, res){
+
+                  message = {
+                    sender_id: users[3].id,
+                    receiver_ids: [users[1].id, users[2].id],
+                    content: "message to tom and sally",
+                    title: "from jill, yerba buena",
+                    latlng: {"lat":37.784775,"lng":-122.402490}
+                  };
+                  request(oaktree.server).post('/message')
+                    .send(JSON.stringify(message))
+                    .end(function(err, res){
+                      console.log("messages sent");
+                    });
+                });
+            });
+        });
     });
 };
 
